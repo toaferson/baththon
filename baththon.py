07d92d87307233332f27c856c0e8dbbfa8c13f75
toaferson <54555471+toaferson@users.noreply.github.com>
2023-07-07 21:40:44 -0400

    Add files via upload

---
+++ baththon.py
@@ -0,0 +1,47 @@
+
+import time
+
+def pyramid():
+
+ while True:
+    print("The Pyramids of Giza are Unknown to mankind itself")
+
+def pbm():
+    x=input('Enter Prompt\n')
+    if x == 'Sound Buying':
+        print("https://youtu.be/oL2B-AAnsHo")
+
+    elif x == 'angeld23':
+     print("hate that egotistical prick")
+
+# repeats for funnies
+    elif x == 'Where are the Pyramids of Giza':
+     pyramid()
+
+    elif x == 'end':
+     return
+
+    else:
+      pbm()
+
+ #random sentencr I made
+x=input("hi guys baby crying pics here\n")
+if x == 'Super Mario Brothers U':
+  print("Mario Bros 2")
+elif x == '3.1415':
+   print("ENTERING Peter Baeten Mode")
+  
+   time.sleep(5)
+   print("S T A R T I N G")
+   time.sleep(1)
+   print("W e l c o m e")
+   time.sleep(1)
+   pbm()
+
+
+  # fly me to the moon 2020
+else:
+  print("https://youtu.be/J07vEAGtm8U%22")
+
+
+ 
